6b6e0c4167f410f9089d64af96ee71394c9c08f6
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 05:31:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,5 +85,16 @@ class Student
     DB[:conn].execute(sql, number).collect{|row|
       self.new_from_db(row)}
   end 
+  
+  def self.first_student_in_grade_10 
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    SQL
+    
+    DB[:conn].execute(sql).collect{|row|
+      self.new_from_db(row)}.first
+  end 
     
 end
