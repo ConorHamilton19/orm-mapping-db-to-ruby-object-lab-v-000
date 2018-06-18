91880ca00a12f6dce738bb3242fdf47ca0ff5e8b
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 05:32:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,7 @@ class Student
       self.new_from_db(row)}
   end 
   
-  def self.first_x_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
     SELECT *
     FROM students
