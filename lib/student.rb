3fcae1b7ac8c025ac0a9bada06d24d4cd27ab793
Conor Hamilton <conhamil19@gmail.com>
2018-06-18 05:30:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,8 @@ class Student
     SELECT *
     FROM students
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect{|row|
+      self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
